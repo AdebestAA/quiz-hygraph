40029815b972afae551dc218021a7db3ba5b1a29
Adebest <aadebesta@gmail.com>
2024-10-18 22:26:34 +0100

    maybe

--- src/app/itp-webhooks/page.js
+++ src/app/itp-webhooks/page.js
@@ -24,7 +24,7 @@ const page = async() => {
           // }
           
           const data = await response.json();
-          console.log(data);
+          // console.log(data);
 
   return (
     <ItpWebhooks dataFromTypeForm={data} />
