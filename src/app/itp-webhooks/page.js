1cc71392a979b12060b1f6b2f017f79b68e2ab3d
Adebest <aadebesta@gmail.com>
2024-10-19 14:11:29 +0100

    again

--- src/app/itp-webhooks/page.js
+++ src/app/itp-webhooks/page.js
@@ -1,34 +1,70 @@
 
-import AhusPostTest from "@/components/AhusPostTest";
 import ItpWebhooks from "@/components/ItpWebhooks";
+import React from 'react'
 
 
 
-    const apiKey = 'tfp_7jNiNB7pBsroNt9fvjLHSWMLpkYRjABEpfDH888YB2Qp_hkTkjbhU58VN';
-    const endpoint = `https://api.typeform.com/forms/YpYxShez`;
+    // const apiKey = 'tfp_7jNiNB7pBsroNt9fvjLHSWMLpkYRjABEpfDH888YB2Qp_hkTkjbhU58VN';
+    // const endpoint = `https://api.typeform.com/forms/YpYxShez`;
 
 const page = async() => {
 
- const response = await fetch(endpoint, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${apiKey}`,
-                'Content-Type': 'application/json'
-            }
-        });
-        console.log(response);
+//  const response = await fetch(endpoint, {
+//             method: 'GET',
+//             headers: {
+//                 'Authorization': `Bearer ${apiKey}`,
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+//         // console.log(response);
         
         
-        // if (!response.ok) {
-          //     throw new Error(`Error: ${response.statusText}`);
-          // }
+//         if (!response.ok) {
+//               throw new Error(`Error: ${response.statusText}`);
+//           }
           
-          const data = await response.json();
-          // console.log(data);
+//           const data = await response.json();
+//           // console.log(data);
 
   return (
-    <ItpWebhooks dataFromTypeForm={data} />
+ <div>
+  still working on it....
+ </div>
   )
 }
 
+
 export default page
+
+
+
+
+
+// const apiKey ='tfp_7jNiNB7pBsroNt9fvjLHSWMLpkYRjABEpfDH888YB2Qp_hkTkjbhU58VN'; 
+// const endpoint = `https://api.typeform.com/forms/YpYxShez`;
+
+// export const getStaticProps = async () => {
+//   const response = await fetch(endpoint, {
+//     method: 'GET',
+//     headers: {
+//       'Authorization': `Bearer ${apiKey}`,
+//       'Content-Type': 'application/json'
+//     }
+//   });
+
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       dataFromTypeForm: data
+//     },
+//     // Optionally, you can set revalidate to regenerate the page after a specified number of seconds
+//     revalidate: 10, // Rebuild the page every 10 seconds
+//   };
+// };
+
+// const Page = ({ dataFromTypeForm }) => {
+//   return <ItpWebhooks dataFromTypeForm={dataFromTypeForm} />;
+// };
+
+// export default Page;
